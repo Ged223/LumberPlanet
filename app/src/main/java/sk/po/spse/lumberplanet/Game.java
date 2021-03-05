24c8bd151c74b12dc5c3e832b65f839ec07a2149
Ged223 <gedsvk@gmail.com>
2021-03-05 20:07:19 +0100

    craft and sell buttons functional

--- app/src/main/java/sk/po/spse/lumberplanet/Game.java
+++ app/src/main/java/sk/po/spse/lumberplanet/Game.java
@@ -1,32 +1,37 @@
 package sk.po.spse.lumberplanet;
 
 public class Game {
-    //in cents, 100 = $1.00
-    int money;
-    int toothpicks;
+
+    private int money;
+    private int toothpicks;
 
     public Game(int money, int toothpicks) {
         this.money = money;
         this.toothpicks = toothpicks;
     }
 
-    public Game(){
-        this(0,0);
+    public Game() {
+        this(0, 0);
+    }
+
+    public void craftToothpick() {
+        this.toothpicks++;
+    }
+
+    public boolean sellToothpick() {
+        if (toothpicks <= 0) {
+            return false;
+        }
+        this.toothpicks--;
+        this.money++;
+        return true;
     }
 
     public int getMoney() {
         return money;
     }
 
-    public void setMoney(int money) {
-        this.money = money;
-    }
-
     public int getToothpicks() {
         return toothpicks;
-    }
-
-    public void setToothpicks(int toothpicks) {
-        this.toothpicks = toothpicks;
     }
 }
--- app/src/main/java/sk/po/spse/lumberplanet/MainActivity.java
+++ app/src/main/java/sk/po/spse/lumberplanet/MainActivity.java
@@ -3,14 +3,39 @@ package sk.po.spse.lumberplanet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private Game game;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //jeden riadok
+
         game = new Game();
+        updateDisplay();
+    }
+
+    public void craftButton(View view) {
+        game.craftToothpick();
+        updateDisplay();
+    }
+
+    public void sellButton(View view) {
+        game.sellToothpick();
+        updateDisplay();
+    }
+
+    private void updateDisplay() {
+        TextView moneyText = findViewById(R.id.moneyText);
+        moneyText.setText(getString(R.string.moneyText) + game.getMoney());
+
+        TextView toothpicksText = findViewById(R.id.toothpicksText);
+        toothpicksText.setText(getString(R.string.toothpicksText) + game.getToothpicks());
     }
 }
